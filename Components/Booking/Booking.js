282cd5d5329e2fdb980c1859a654119ad432aa1e
Errytagedesign <errytage@gmail.com>
2023-07-22 11:50:42 +0100

    Add services pages

---
+++ Components/Booking/Booking.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import styles from './Booking.module.scss';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { FiMail } from 'react-icons/fi';
+
+function Booking() {
+  const service = [
+    { name: ' Work visas assistance ', url: '/workvisas' },
+    {
+      name: 'Study visa assistance        ',
+      url: '/studentvisas',
+    },
+    {
+      name: 'Permanent residents visas',
+
+      url: '/permanentvisas',
+    },
+    {
+      name: ' Citizenship visas assistance',
+      url: '/citizenshipvisas',
+    },
+    {
+      name: 'Business visas assistance',
+
+      url: '/businessvisas',
+    },
+    { name: 'Tourist visa assistance', url: '/touristvisas' },
+    {
+      name: 'Flight tickets assistance.',
+      url: '/hotelticket',
+    },
+    {
+      name: 'Hotels and apartment reservation.        ',
+      url: '/hotelreservation',
+    },
+  ];
+  return (
+    <main className='mb-5'>
+      <section className='border  py-5 px-3 mb-5'>
+        <h3>Book a service</h3>
+        <form>
+          <input
+            type='text'
+            placeholder='Your name'
+            className='form-control mb-3'
+            required
+          />
+          <input
+            type='email'
+            placeholder='Your email'
+            className='form-control mb-3'
+            required
+          />
+          <input
+            type='number'
+            placeholder='Your number'
+            className='form-control mb-3'
+            required
+          />
+          <input
+            type='date'
+            placeholder='Your number'
+            className='form-control mb-3'
+            required
+          />
+          <select required className='form-control mb-3'>
+            {service.map(({ name }) => (
+              <option> {name} </option>
+            ))}
+          </select>
+          <textarea
+            name='messgae'
+            id='booking-message'
+            cols='30'
+            rows='10'
+            class='form-control mb-3'
+            placeholder='Message *'
+            required
+          ></textarea>
+
+          <div className='col-12'>
+            <button type='submit' className='main-btn mt-3 col-12'>
+              BOOK NOW
+            </button>
+          </div>
+        </form>{' '}
+      </section>
+
+      <section className={`${styles.question} mt-5`}>
+        <h3>Any Question?</h3>
+        <p>
+          If you require additional information on Tours, Ticketing &
+          Reservation, Hotel Reservation, Visa Counseling, Immigration &
+          Protocol Service and any other; kindly contact us:
+        </p>
+        <div className='d-flex flex-column mt-3'>
+          <h5>
+            <BsFillTelephoneFill />{' '}
+            <a href='tel: +2349133214070'>+2349133214070</a>
+          </h5>
+
+          <h5>
+            <BsFillTelephoneFill />
+            <a href='tel: +2349022113315'>+2349022113315</a>
+          </h5>
+
+          <p>
+            <FiMail />
+            <a href='mailto: info@destineytravelandtours.com'>
+              info@destineytravelandtours.com
+            </a>
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Booking;
---
+++ app/citizenshipvisas/page.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './workvisa.module.scss';
+import Booking from '@/Components/Booking/Booking';
+
+import work from '../../public/assets/destinytravelworkvisas.jpeg';
+import Image from 'next/image';
+
+export default function WorkVisas() {
+  return (
+    <main className={`${styles.workVisas} `}>
+      {' '}
+      <section
+        className={`${styles.workHero} d-flex justify-content-between align-items-center`}
+      >
+        <h2 className='col-11 col-md-6 mx-auto'>
+          Citizenship visa application assistance
+        </h2>
+      </section>
+      <article className=' d-flex flex-column flex-lg-row justify-content-between mt-5 '>
+        <section className='container mt-5 col-12 col-lg-7 '>
+          <p>
+            {' '}
+            Welcome to Destiny Travel and Tours, your trusted partner in
+            realizing your career aspirations abroad.
+            <br /> <br /> Our comprehensive Work Visa Applications Assistance
+            service is designed to make your international job opportunities a
+            reality. Navigating the intricacies of work visa applications can be
+            daunting, but with our expert guidance, you can rest assured that
+            every detail will be handled with precision. <br /> <br />
+            Our seasoned visa experts will walk you through the entire process,
+            starting with understanding your career goals and the specific
+            requirements of your chosen destination. We will meticulously assist
+            you in gathering all necessary documents, filling out forms
+            accurately, and meeting the eligibility criteria. With our
+            personalized approach, we ensure that your work visa application
+            stands out among the competition, significantly increasing your
+            chances of obtaining the visa.
+          </p>
+          <div className='col-12'>
+            <Image src={work} alt='' />
+          </div>
+          <button className='main-btn my-5'> Contact us</button>
+        </section>
+        <aside className='col-12 col-lg-4 container'>
+          <Booking />
+        </aside>
+      </article>
+    </main>
+  );
+}
